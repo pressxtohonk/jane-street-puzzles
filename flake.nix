0fd02db51b966ccff05fb0949e6049bbc7ad3527
pressxtohonk <pressxtohonk@gmail.com>
2025-05-01 02:15:53 +0800

    Set up dev env for nix

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Python notebook environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in 
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          packages = with pkgs; [ 
+            python312 
+            pyright
+            ruff
+          ] ++ (with pkgs.python312Packages; [
+            pip
+            ipykernel
+            jupyter
+            numpy
+            ortools
+            pandas
+            plotly
+            protobuf
+            pydantic
+            seaborn
+            scipy
+            matplotlib
+            seaborn
+            venvShellHook
+          ]);
+        };
+      });
+    };
+}
